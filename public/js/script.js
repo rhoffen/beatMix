85a4699928cff9518988b937fe5d195ed0dd76ef
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-05-02 11:12:47 -0700

    First commit - tests of array existence should pass but they're not.

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// Drum Arrays
+let kicks = Array(16).fill(false);
+let snares = Array(16).fill(false);
+let hiHats = Array(16).fill(false);
+let rideCymbals = Array(16).fill(false);
+
+console.log(kicks.length);
+console.log(kicks);
